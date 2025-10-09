ec28a95cd9a40e8d36d7b0a7c37eee6a92bb87a6
aShinde <ananta.shinde@honeywellcom>
2025-10-09 18:44:26 +0530

    09/10/2025

--- src/advance/ds/MyLinkedList.java
+++ src/advance/ds/MyLinkedList.java
@@ -16,7 +16,25 @@ public class MyLinkedList {
         if(head == null){
             System.out.println("list is empty");
         }else{
+            Node temp = head;
+            while(temp != null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
 
+    public int size(){
+        if(head == null){
+            return 0;
+        }else{
+            Node temp = head;
+            int count = 0;
+            while(temp != null){
+                count++;
+                temp = temp.next;
+            }
+            return count;
         }
     }
 
@@ -38,7 +56,83 @@ public class MyLinkedList {
         newNode.next = head;
         head = newNode;
     }
-    public static void main(String[] args) {
 
+
+
+    public void insert(int index,int value){
+        Node newNode = new Node(value);
+        if(head == null){
+            head = newNode;
+        }else{
+            Node temp = head;
+            int count = 1;
+            while(count<index) {
+                temp = temp.next;
+                count++;
+            }
+            newNode.next =temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    public void delete(int index){
+
+        if(head == null){
+
+        }else if(index == 0) {
+            head = head.next;
+        }else {
+            Node temp = head;
+            int count = 1;
+            while (temp != null && count < index){
+
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+
+        }
+    }
+
+    public int indexof(int value){
+        if(head == null){
+            return -1;
+        }else{
+            Node temp = head;
+            int count = -1;
+            while(temp != null){
+                count++;
+                if(temp.data == value){
+                    return count;
+                }
+                temp = temp.next;
+            }
+            return count;
+        }
+    }
+
+    public void deleteByValue(int value){
+        if(head == null){
+
+        }else if(head.data == value) {
+            head = head.next;
+        }else {
+            Node temp = head;
+            while (temp.next.data != value){
+                temp = temp.next;
+            }
+            temp.next=temp.next.next;
+        }
+    }
+    public static void main(String[] args) {
+            MyLinkedList list = new MyLinkedList();
+            list.insert(20);
+            list.addFirst(35);
+            list.insert(30);
+            list.insert(2,45);
+//            list.deleteByValue(35);
+             list.delete(2);
+            System.out.println("index : "+list.indexof(45));
+            System.out.println("size :"+list.size());
+            list.print();
     }
 }
